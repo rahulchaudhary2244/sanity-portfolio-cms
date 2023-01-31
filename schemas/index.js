3ba77acdd8d178c2523eebe8a25db2d39b1f815f
rahulchaudhary2244 <rahulchaudhary2244@gmail.com>
2023-01-31 20:40:28 +0530

    initial commit

--- schemas/index.js
+++ schemas/index.js
@@ -1,6 +1,7 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import pageInfo from './pageInfo';
+import experience from './experience';
+import project from './project';
+import skill from './skill';
+import social from './social';
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [pageInfo, experience, skill, social, project];
---
+++ schemas/skill.js
@@ -0,0 +1,28 @@
+import { defineType } from 'sanity';
+
+export default defineType({
+    name: 'skill',
+    title: 'Skill',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            description: 'Name of the skill',
+            type: 'string',
+        },
+        {
+            name: 'progress',
+            title: 'Progress',
+            type: 'number',
+            description: 'Progress of the skill from 0 to 100',
+            validation: (value) => value.min(0).max(100),
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            options: { hotspot: true },
+        },
+    ],
+});
